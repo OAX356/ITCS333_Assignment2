b7bb1145057e61d1ff7f4e9e0e7d6387678ac753
OAX356 <omaradnan2003@gmail.com>
2024-11-26 19:26:58 +0300

    Update data_retrieval.php

--- data_retrieval.php
+++ data_retrieval.php
@@ -1,1 +1,29 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University of Bahrain Students Enrollment</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <div class="overflow-auto">
+            <!-- Table to display data -->
+            <table>
+                <thead data-theme="light">
+                  
+                </thead>
+                <tbody data-theme="light">
+                    
+                </tbody>
+            </table>
+        </div>
+    </main>
+</body>
+</html>
